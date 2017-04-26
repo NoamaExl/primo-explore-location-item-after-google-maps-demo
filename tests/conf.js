82c5982f089543a9bafd336a4fbd27ca86da6fc9
Noam Amit <noamamit92@gmail.com>
2017-04-26 17:00:09 +0300

    Update conf.js

--- tests/conf.js
+++ tests/conf.js
@@ -1,4 +1,14 @@
 exports.config = {
+    onPrepare: function() {
+        // implicit and page load timeouts
+        browser.manage().timeouts().pageLoadTimeout(40000);
+        browser.manage().timeouts().implicitlyWait(25000);
+
+        // for non-angular page
+        browser.ignoreSynchronization = true;
+
+    
+    },
     specs: ['todo-spec.js'],
     allScriptsTimeout: 100000,
     // Options to be passed to Jasmine-node.
