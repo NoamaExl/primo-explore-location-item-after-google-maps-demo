0349878dddd9c56e4a62e36825c4c234f0289861
Noam Amit <noamamit92@gmail.com>
2017-04-27 17:03:14 +0300

    Update primo-explore-location-item-after.js

--- js/primo-explore-location-item-after.js
+++ js/primo-explore-location-item-after.js
@@ -33,22 +33,9 @@ app.controller('prmLocationItemAfterController', [ '$sce', 'angularLoad', functi
 
 
     vm.geoMapping = {
-        '(stack no.118 )NBPER': '52.938960, -1.196906',
-        'NVET': '52.938960, -1.196906',
-        '(289.6 9ZAR10D11S05TBT F906 )SMLAC': '52.938960, -1.196906',
-        '(301.06 9ZAR03D01S06TFW T573 )SMLAC': '52.938942, -1.197074',
-        '(Z286.S37 S38x 1997 )NWILS': '52.938764, -1.197330',
-        '(Journal of cell science. ; 4. )NBPER': '52.939000, -1.197417',
-        '(JK141 1893 )MAIN': '52.938960, -1.196906',
-        '(320.975 A23 )MAIN': '52.938960, -1.196906',
-        '(Journal of otolaryngology. Supplement ; no. 6. )NBPER': '52.938960, -1.196906',
-        '(Journal of endocrinology ; v. 155, suppl. 1. )NBPER': '52.938942, -1.197074',
-        '(Journal of rheumatology. Supplement ; no. 51 )NBPER': '52.938764, -1.197330',
-        'NWPR': '52.938960, -1.196906',
-        '(Mfiche 2 Journal of chemical research. Synopses. )NSCI': '52.938960, -1.196906',
-        '(Quarto QD1 .J9425x )NSCI': '52.938942, -1.197074',
-        '(Quarto QD1 .J9425x )NINTE': '52.938764, -1.197330',
-        '(Quarto QD1 .J9425x )NMAGR': '52.939000, -1.197417'
+        '(stack no.127 )NBPER': '52.938960, -1.196906',
+        '(864.a.421(30) )44CAM_864A+85Z2': '52.938942, -1.197074',
+
     };
 
 
