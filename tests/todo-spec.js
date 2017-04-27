70b45b7820bfbf3ddd63fbeac8db5aed2d5972b5
Noam Amit <noamamit92@gmail.com>
2017-04-27 17:02:47 +0300

    Update todo-spec.js

--- tests/todo-spec.js
+++ tests/todo-spec.js
@@ -3,22 +3,9 @@ const protractorImageComparison = require('protractor-image-comparison');
 
 describe('test primo-explore-location-item-after', function() {
     let geoMapping = {
-        '(stack no.118 )NBPER': '52.938960, -1.196906',
-        'NVET': '52.938960, -1.196906',
-        '(289.6 9ZAR10D11S05TBT F906 )SMLAC': '52.938960, -1.196906',
-        '(301.06 9ZAR03D01S06TFW T573 )SMLAC': '52.938942, -1.197074',
-        '(Z286.S37 S38x 1997 )NWILS': '52.938764, -1.197330',
-        '(Journal of cell science. ; 4. )NBPER': '52.939000, -1.197417',
-        '(JK141 1893 )MAIN': '52.938960, -1.196906',
-        '(320.975 A23 )MAIN': '52.938960, -1.196906',
-        '(Journal of otolaryngology. Supplement ; no. 6. )NBPER': '52.938960, -1.196906',
-        '(Journal of endocrinology ; v. 155, suppl. 1. )NBPER': '52.938942, -1.197074',
-        '(Journal of rheumatology. Supplement ; no. 51 )NBPER': '52.938764, -1.197330',
-        'NWPR': '52.938960, -1.196906',
-        '(Mfiche 2 Journal of chemical research. Synopses. )NSCI': '52.938960, -1.196906',
-        '(Quarto QD1 .J9425x )NSCI': '52.938942, -1.197074',
-        '(Quarto QD1 .J9425x )NINTE': '52.938764, -1.197330',
-        '(Quarto QD1 .J9425x )NMAGR': '52.939000, -1.197417'
+        '(stack no.127 )NBPER': '52.938960, -1.196906',
+        '(864.a.421(30) )44CAM_864A+85Z2': '52.938942, -1.197074',
+
     };
 
     it('General snapshot regression tests', function() {
@@ -26,19 +13,12 @@ describe('test primo-explore-location-item-after', function() {
         browser.waitForAngular();
         browser.sleep(10000);
 
-        expect(browser.protractorImageComparison.checkScreen('locationsMaps')).not.toBeCloseTo(1);
+        expect(browser.protractorImageComparison.checkScreen(browser.params.scenario)).toBeCloseTo(0,1);
     });
 
-
-
     it('The following should show a google maps iframe consistent with the location library', function() {
-        browser.get('http://localhost:8003/primo-explore/search?query=any,contains'+browser.params.query+'&vid='+browser.params.vid+'&lang=en_US&offset=0&noSilentLogin=true');
-        //browser.sleep(3000);//wait for sso
+        browser.get('http://localhost:8003/primo-explore/search?query=any,contains,'+browser.params.query+'&vid='+browser.params.vid+'&lang=en_US&offset=0&noSilentLogin=true');
         browser.waitForAngular();
-        /*element(by.css('#searchBar')).clear().sendKeys('Journal of');/!*Blue_Bay_Aleph001886410*!/
-         browser.actions().sendKeys(protractor.Key.TAB).perform();
-         element(by.css('.search-actions .button-confirm')).click();*/
-
         element.all(by.tagName('prm-search-result-availability-line')).each(function(e1){
             browser.waitForAngular();
             e1.all(by.css('.arrow-link-button')).each(function(e2){
@@ -50,11 +30,7 @@ describe('test primo-explore-location-item-after', function() {
                             let geoLocation = geoMapping[browser.params.loc] || '40.689237, -74.044546';
                             /**********This is the actual tested logic end**********/
                             element.all(by.tagName('prm-location-items-after')).each(function(e3){
-                                //browser.waitForAngular();
-                                //browser.sleep(10000);
                                 e3.element(by.tagName('iframe')).getAttribute('src').then(function(source){
-                                    //browser.waitForAngular();
-                                    //browser.sleep(10000);
                                     console.log(source);
                                     /**********This is the assertion part start**********/
                                     expect(source).toContain(encodeURI(geoLocation));
